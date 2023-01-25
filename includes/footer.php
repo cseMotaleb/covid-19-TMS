c93fc6d6c8dfe4ad2bbc3d90ccd49c1de4942050
cseMotaleb <csemotaleb@gmail.com>
2023-01-25 20:13:24 +0600

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Covid-19 Testing Management System 2023</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid-19 Testing Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+<style>
+  li.nav-item {
+      padding: 10px 13px;
+  }
+#mainNav a.nav-link{
+  font-size: 16px;
+  text-transform: uppercase;
+  color: #333;
+}
+.navbar-dark .navbar-brand {
+    color: #333;
+}
+.navbar-dark .navbar-brand:focus, .navbar-dark .navbar-brand:hover{
+  color: #000;
+}
+
+</style>
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-white fixed-top shadow" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Covid-19 TMS</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class=" text-white" style="background: #7258db; padding: 213px 0 100px">
+    <div class="container text-center">
+      <h1>COVID-19 TMS</h1>
+      <p class="lead">COVID-19  Testing Management System</p>
+      <p></p>
+      <p> Viral tests, NAATs test, Antigen tests, Self-tests</p>
+
+        <div class="text-center about-section mt-20">
+    <h2 style="margin-top: 20px;">When to Get Tested for COVID-19</h2>
+    <p><strong>Key times to get tested:</strong>
+
+      If you have symptoms, test immediately.
+      If you have symptoms, test immediately.
+      If you were exposed to COVID-19 and do not have symptoms, wait at least 5 full days after your exposure before testing.  If you test too early, you may be more likely to get an inaccurate result.
+      
+      If you are in certain high-risk settings, you may need to test as part of a screening testing program.
+      Consider testing before contact with someone at high risk for severe COVID-19, especially if you are in an area with a medium or high COVID-19 Community Level.
+    </p>
+   
+  </div>
+    </div>
+
+  </header>
+
+
+  
+
+  
+  
+
+  <!-- Footer -->
+  <footer class="py-5" style="background: #f2f2f2; position: absolute; bottom: 0px; width: 100%;">
+    <div class="container">
+      <p class="m-0 text-center text-black">Copyright &copy; Covid-19 Testing Management System 2023</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
